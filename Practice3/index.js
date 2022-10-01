e79b5030e7a9e1896f3ed5e34a30aa151ccf0b42
Orxan223 <or.xan.2018@bk.ru>
2022-10-01 15:17:33 +0300

    finish MineSweeper

--- Practice3/index.js
+++ Practice3/index.js
@@ -1,16 +1,46 @@
-const boxes = document.querySelectorAll(" .box")
+class MineSweeper {
+    qutular = document.querySelectorAll('#boxes .box');
+    minalar = [];
+    oyunBitib = false;
 
+    constructor () {
+        this.minalariDuz();
+        console.log(this.minalar);
+        this.minalarinSayiniGoster();
+        this.qutularaClickleriYapishdir();
+    }
 
-function rengiDeyis(qutu){
-    if (qutu.classList.contains('qirmizi')) {
-        qutu.classList.remove('qirmizi')
-    } else {
-        qutu.classList.add('qirmizi')
-    }       
+    minalariDuz () {
+        for(let i = 0; i < this.qutular.length; i++) {
+            const tesadufiMina = Math.round(Math.random()) === 1;
+            this.minalar.push(tesadufiMina);
+        }
+    }
+
+    minalarinSayiniGoster () {
+        document.querySelector('#say').innerHTML = this.minalar.filter(x => x).length;
+        document.querySelector('#temiz-say').innerHTML = this.minalar.filter(x => !x).length;
+    }
+
+    qutularaClickleriYapishdir () {
+        for(let i = 0; i < this.qutular.length; i++) {
+            const qutu = this.qutular[i];
+
+            qutu.addEventListener('click', (e) => {
+                if (this.oyunBitib) {
+                    alert("Oyun bitib!!!");
+                    return;
+                }
+
+                if (this.minalar[i]) {
+                    e.target.classList.add('qirmizi');
+                    this.oyunBitib = true;
+                } else {
+                    e.target.classList.add('yashil');
+                }
+            });
+        }
+    }
 }
 
-for (let box of boxes) {
-    box.addEventListener('click', function (e) {
-        rengiDeyis(e.target)
-    })
-}
+const game = new MineSweeper();
